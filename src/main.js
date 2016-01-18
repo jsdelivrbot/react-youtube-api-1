fbc1ac8682db7912c9e37eedd26cf02cfce2023e
Salman Akram <salmanit2012@gmail.com>
2016-01-18 00:11:33 -0500

    create controlled components

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';  // Youtube API Search
 import {API_KEY} from '../keys';
+import SearchBar from './components/search_bar';  // from components file
 
-import SearchBar from './components/search_bar';
+// Search response
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({
+      key: API_KEY, term: 'books'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
